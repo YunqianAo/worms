896ff5c11539e33d8f3a4e085af15930f7b24dab
YunqianAo <79161526+YunqianAo@users.noreply.github.com>
2021-12-14 15:59:56 +0100

    Matrix.cpp

---
+++ Physics II - Ejercicios/Matrix.cpp
@@ -0,0 +1,10 @@
+#include"Matrix.h"
+
+namespace E3D {
+	EMatrix44 EMatrix44::IDENTITY(
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+		);
+}
--- Physics II - Ejercicios/Matrix.h
+++ Physics II - Ejercicios/Matrix.h
@@ -67,7 +67,22 @@ namespace E3D {
 			}
 			return temp;
 		}
-		
+		inline EMatrix44& operator=(const EMatrix44& right) {
+			m[0][0] = right.m[0][0], m[0][1] = right.m[0][1], m[0][2] = right.m[0][2], m[0][3] = right.m[0][3];
+			m[1][0] = right.m[1][0], m[1][1] = right.m[1][1], m[1][2] = right.m[1][2], m[1][3] = right.m[1][3];
+			m[2][0] = right.m[2][0], m[2][1] = right.m[2][1], m[2][2] = right.m[2][2], m[2][3] = right.m[2][3];
+				m[3][0] = right.m[3][0], m[3][1] = right.m[3][1], m[3][2] = right.m[3][2], m[3][3] = right.m[3][3];
+		}
+		inline bool operator==(const EMatrix44& right) const {
+			if (m[0][0] != right.m[0][0] || m[0][1] != right.m[0][1] || m[0][2] != right.m[0][2] || m[0][3] != right.m[0][3] ||
+				m[1][0] != right.m[1][0] || m[1][1] != right.m[1][1] || m[1][2] != right.m[1][2] || m[1][3] != right.m[1][3] ||
+				m[2][0] != right.m[2][0] || m[2][1] != right.m[2][1] || m[2][2] != right.m[2][2] || m[2][3] != right.m[2][3] ||
+				m[3][0] != right.m[3][0] || m[3][1] != right.m[3][1] || m[3][2] != right.m[3][2] || m[3][3] != right.m[3][3]) {
+				return false;
+			}
+			return true;
+		}
+
 	};
 
 }
